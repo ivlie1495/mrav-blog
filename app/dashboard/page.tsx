e600d073d195ef7b412b330eea11ce978f333c8f
ivlie7 <ivlie1495@gmail.com>
2024-08-24 08:31:00 +0700

    setup kinde

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Hello from dashboard page</div>
+}
+
+export default Page
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+
 import { Button } from '@/components/ui/button'
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession()
+  const session = await getUser()
+
   return (
-    <div>
-      <Button>Test</Button>
+    <div className="p-6 space-x-2">
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ) : (
+        <>
+          <RegisterLink>
+            <Button>Register</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Login</Button>
+          </LoginLink>
+        </>
+      )}
     </div>
   )
 }
