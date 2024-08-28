0f5bb9eafa209049ce0c6a052a507e599bdbc49b
ivlie7 <ivlie1495@gmail.com>
2024-08-28 09:02:32 +0700

    render children layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -29,6 +29,9 @@ const Layout = ({ children }: PropsWithChildren) => {
             <AccountMenu />
           </div>
         </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          {children}
+        </main>
       </div>
     </section>
   )
